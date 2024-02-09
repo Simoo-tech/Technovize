875e6cd63e268cf5f4b68370e5ce40fc5f8fdd6f
Ahmed123Hamada <wasdahmed033@gmail.com>
2024-02-09 17:34:59 +0200

    last update on about Us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,56 +1,64 @@
 import React from "react";
-import { BiShieldAlt2, BiSupport } from "react-icons/bi";
-import { FaCoins, FaRegLightbulb } from "react-icons/fa";
-import { GiRibbonMedal } from "react-icons/gi";
 import { IoIosArrowBack } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { Cards } from "../components/Cards";
-
+import imgeStrategic from "../assets/est.jpg";
+import ContactEmergencyIcon from "@mui/icons-material/ContactEmergency";
+import AutoGraphIcon from "@mui/icons-material/AutoGraph";
+import AdsClickSharpIcon from "@mui/icons-material/AdsClickSharp";
 function About() {
   return (
     <>
-      <div className=" about relative h-[60vh] bg-gray-500  bg-blend-overlay flex justify-between items-center"></div>
+      <div className=" about relative h-[60vh] bg-gray-400  bg-blend-overlay flex justify-between items-center"></div>
 
       {/* about company... */}
       <div className="container  top-0 right-0 absolute">
         <div
-          className=" h-[60vh] flex flex-col  gap-10 justify-center
+          className=" h-[60vh] flex flex-col  gap-10 justify-center px-4
                 sm:flex-col sm:items-center 
-                md:justify-center md:items-center
+                md:justify-center md:items-start
                 lg:justify-center lg:items-start
         "
         >
-          <h1 className="text-6xl text-white font-bold">عن شركة . . .</h1>
+          <h1 className="text-6xl font-extrabold text-color3 3  ">تكنو فايز</h1>
           <h3 className="text-2xl text-white">
             نحن معك خطوة بخطوة لتحويل فكرتك الي بيزنيس حقيقي
           </h3>
         </div>
       </div>
       {/* .......about company end.... */}
-      <div className="container bottom-40 relative  ">
-        <div className=" bg-[#2F2D31] flex flex-col justify-center items-center rounded-xl py-10 container gap-10">
+      <div className="container bottom-28  relative  ">
+        <div className=" bg-[#252427e3] flex flex-col justify-center items-center rounded-xl py-12 px-10 container gap-14">
           {/* .......about... */}
           <div
-            className="flex  flex-col justify-center  gap-10  text-white 
-                          max-lg:items-center
-                          max-md:gap-16          
-                          "
+            className="flex  justify-between  items-center   text-white
+            max-lg:items-center lg:mt-16
+          max-md:mt-10   lg:w-8/12      
+          "
           >
-            <h1 className="text-6xl font-bold">من نحن . .</h1>
             <div
-              className="flex gap-5 w-full
-            max-lg:flex-col max-lg:items-center
-            max-md:gap-10
-
+              className="flex  flex-col gap-4 
+               max-md:items-Center *:md:mt-6 
             "
             >
+              <h3
+                className="text-4xl font-extrabold flex items-center gap-4
+            sm:text-3xl  sm:items-center  
+            "
+              >
+                
+                من نحن
+                <ContactEmergencyIcon
+                  fontSize="large"
+                  className="text-color3 "
+                />
+              </h3>
               <p
-                className="text-lg
-                 sm:px-3
-                md:text-base md:px-5 lg:text-xl
-               lg:px-10
-                text-pretty 
-              "
+                className="text-lg 
+                sm:text-base sm:mr-4 sm:items-start  
+                md:text-base md:w-10/12 
+                lg:w-9/12
+                "
               >
                 شركة تكنو فايز هي احدي اكبر شركات البرمجة في مصر و الوطن العربي
                 معنا يمكنك تحويل مشروعك من مجرد فكرة الي حقيقة من خلال تطوير
@@ -64,32 +72,53 @@ function About() {
                 className="flex w-fit py-2 sm:text-base md:text-lg px-6 items-center gap-2 bg-orange-900 text-white group
           duration-300 rounded-l-2xl hover:bg-color3 hover:rounded-md hover:-translate-x-2
           
-          md:h-[60px]          "
+          md:h-[60px]    
+          sm:mr-4  sm:h-[35px]
+                "
               >
                 المزيد عنا
-                <IoIosArrowBack className="group-hover:animate-rightLeft" />
+                <IoIosArrowBack className="group-hover:animate-rightLeft " />
               </Link>
             </div>
+            <img
+              className="w-4/12   rounded-xl  max-md:hidden"
+              src={imgeStrategic}
+              alt="imgeStrategic"
+            />
           </div>
           {/* .....goales... */}
           <div
-            className="flex flex-col justify-center px-10 gap-10 text-white
-                          max-lg:items-center
-                          max-md:gap-16          
-
+            className="flex  justify-evenly items-center gap-14  text-white    lg:w-8/12      
+              max-lg:items-center
           "
           >
-            <h1 className="text-6xl font-bold">هدفنا </h1>
+            <img
+              className="w-4/12 max-md:hidden  rounded-xl"
+              src={imgeStrategic}
+              alt=""
+            />
+
             <div
-              className="flex  gap-5 w-[80%]
-            max-lg:flex-col max-lg:items-center
+              className="flex flex-col justify-between gap-8 
+            max-lg:flex-col max-lg:items-start
+            sm:itms-start
             "
             >
-              <p
-                className="text-lg  w-7/12              
-               max-md:text-sm
+              <h3
+                className="  text-4xl font-extrabold items-center flex gap-4
+               sm:items-center
+               max-lg:text-right 
+            "
+              >
+                هدفنا{" "}
+                <AdsClickSharpIcon fontSize="large" className="text-color3" />
+              </h3>
 
-"
+              <p
+                className="text-lg  lg:w-11/12          
+               max-md:text-md
+               sm:text-base 
+               sm:mr-4 sm:items-start"
               >
                 هو تقديم حلول تقنية متقدمة ومبتكرة لتلبية احتياجات عملائها. نسعى
                 لتحويل الأفكار إلى واقع رقمي من خلال تطوير مواقع الويب وتطبيقات
@@ -98,39 +127,37 @@ function About() {
                 بأحدث التقنيات. نحن نسعى جاهدين لتحقيق تجارب مستخدم مميزة
                 وملهمة، مما يسهم في نجاح مشاريع عملائنا وتحقيق رؤى تقنية فريدة.
               </p>
-              <Link
-                to={""}
-                className="flex w-fit py-2 sm:text-base md:text-lg px-6 items-center gap-2 bg-orange-900 text-white group
-          duration-300 rounded-l-2xl hover:bg-color3 hover:rounded-md hover:-translate-x-2
-          
-          md:h-[60px]
-          "
-              >
-                المزيد عنا
-                <IoIosArrowBack className="group-hover:animate-rightLeft" />
-              </Link>
             </div>
           </div>
-          {/* .....estrategy... */}
-          <div
-            className="flex flex-col justify-center mb-16 px-10 gap-10  text-white
-                                    max-lg:items-center
-                                    max-md:mt-14         
 
+          {/* .....Strategic... */}
+          <div
+            className="flex  justify-between  items-center   text-white
+              max-lg:items-center
+              max-md:mt-14   lg:w-8/12      
           "
           >
-            <h1 className="text-6xl font-bold">استراجياتنا </h1>
             <div
-              className="flex  gap-5 w-[80%]
-                        max-lg:flex-col max-lg:items-center
-
+              className="flex  flex-col gap-8 
+               max-md:items-Center
             "
             >
+              <h3
+                className="text-4xl font-extrabold flex items-center gap-4
+            sm:text-center
+             lg:text-start 
+            "
+              >
+                {" "}
+                استراجياتنا{" "}
+                <AutoGraphIcon fontSize="large" className="text-color3" />{" "}
+              </h3>
               <p
-                className="text-lg w-7/12
-                max-md:text-sm
-              
-              "
+                className="text-lg
+                sm:text-base sm:mr-4 sm:items-start  
+                lg:w-10/12
+                md:w-10/12
+                "
               >
                 نقوم بتحليل الاحتياجات: سنقوم بالاستماع إلى متطلباتك وفهم أهدافك
                 ورؤيتك. سنعمل معك على وضع استراتيجية تطوير ملائمة تلبي احتياجاتك
@@ -140,16 +167,12 @@ function About() {
                 سيقدمون افضل الحلول للمشكلات التي ستواجك خلال بدا مشروعك و
                 سيقدمون اقتراحات تجعل مشروعك افضل و اوضح
               </p>
-              <Link
-                to={""}
-                className="flex w-fit py-2 sm:text-base md:text-lg px-6 items-center gap-2 bg-orange-900 text-white group
-          duration-300 rounded-l-2xl hover:bg-color3 hover:rounded-md hover:-translate-x-2
-          md:h-[60px]          "
-              >
-                المزيد عنا
-                <IoIosArrowBack className="group-hover:animate-rightLeft" />
-              </Link>
             </div>
+            <img
+              className="w-4/12   rounded-xl  max-md:hidden"
+              src={imgeStrategic}
+              alt="imgeStrategic"
+            />
           </div>
         </div>
       </div>
