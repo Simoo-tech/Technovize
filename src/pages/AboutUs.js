5efd76a965f14ac08fc6a4be03df13c18862ad1e
Ahmed123Hamada <wasdahmed033@gmail.com>
2024-02-09 17:40:06 +0200

    last update on about Us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -46,7 +46,6 @@ function About() {
             sm:text-3xl  sm:items-center  
             "
               >
-                
                 من نحن
                 <ContactEmergencyIcon
                   fontSize="large"
